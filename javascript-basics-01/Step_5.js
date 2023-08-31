fa3091787e9fb855798d6c9c54f9d52241e533b8
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-08-31 12:48:08 +0300

    finish step 5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+var num1 = prompt("Please give me value of first number: ")
+var num2 = prompt("please give me value of second number :")
+
+var mlt = (num1 * num2) 
+
+window.alert(`result : ${mlt}`)
