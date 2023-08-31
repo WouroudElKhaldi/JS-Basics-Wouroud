3335365886deaab26e400b5fa83c8ab41d98a5c4
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-08-31 12:35:59 +0300

    finish Step 4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+var UserName = window.prompt("Plz enter your Name :")
+var Surname = window.prompt("Plz enter your Surname :")
+var City = window.prompt("Plz enter your City :")
+ alert(`Hello, your name is ${UserName} 
+ Surname : ${Surname}
+ City : ${City}`)
